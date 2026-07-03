87e24a4eb6909d12b5c612f42ed66cc0887103a1
NetAcad DEVASC <changeme@example.com>
2026-07-03 17:08:58 +0000

    Scripts Item 1

---
+++ integrantes.py
@@ -0,0 +1,3 @@
+integrantes = ["Fabán Alvial", "Felipe Armijo", "Nicolas Farias", "Diego Marchant"]
+for i in integrantes:
+    print(i)
---
+++ vlan.py
@@ -0,0 +1,7 @@
+vlan = int(input("Ingrese el número de VLAN: "))
+if 1 <= vlan <= 1005:
+    print("VLAN de rango normal")
+elif 1006 <= vlan <= 4094:
+    print("VLAN de rango extendido")
+else:
+    print("Número inválido")
